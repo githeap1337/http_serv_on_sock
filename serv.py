27e129947826f7cc8eed9d7fa725301fe7486d9d
Artem Galichanin <githeap1337@gmail.com>
2023-01-16 17:36:54 +0300

    delete while loop

--- serv.py
+++ serv.py
@@ -10,19 +10,19 @@ def main():
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     server.listen(10)
-    while True:
-        client_socket, address = server.accept()
-        print(server.accept())
-        received_data = client_socket.recv(1024).decode('utf-8')
-        print('get data from socket: ', received_data)
-        response = 'HTTP/1.1 200 OK\nContent-type: text/html; charset=utf-8\n\n'
-        with open('packeges.txt', 'w') as packs:
-            for pack in packs:
-                response += pack.strip() + '\n'
-        client_socket.send(response.encode('utf-8'))
-        client_socket.shutdown(socket.SHUT_RDWR)
-    # server.shutdown(socket.SHUT_RDWR)
-    # server.close()
+    # while True:
+    client_socket, address = server.accept()
+    print(server.accept())
+    received_data = client_socket.recv(1024).decode('utf-8')
+    print('get data from socket: ', received_data)
+    response = 'HTTP/1.1 200 OK\nContent-type: text/html; charset=utf-8\n\n'
+    with open('packages.txt', 'w') as packs:
+        for pack in packs:
+            response += pack.strip() + '<br />>'
+    client_socket.send(response.encode('utf-8'))
+    client_socket.shutdown(socket.SHUT_RDWR)
+    server.shutdown(socket.SHUT_RDWR)
+    server.close()
 
 
 if __name__ == "__main__":
